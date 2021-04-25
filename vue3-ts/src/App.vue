bc8bd6dd5bf61610e96127e2be0c094b1fbe5891
joshua1988 <jangkeehyo@gmail.com>
2021-04-25 18:14:57 +0900

    add vue3 project

---
+++ vue3-ts/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>{{ str }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  // setup() {
+  //   const str = ref("");
+
+  //   str.value = "hi";
+
+  //   return { str };
+  // },
+  data() {
+    return {
+      str: "",
+    };
+  },
+  methods: {
+    changeString() {
+      this.str = "hi";
+    },
+  },
+});
+</script>
+
+<style scoped></style>
